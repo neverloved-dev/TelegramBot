91ce335f45ab166b1f6f01a107c48588e603af18
Andjic Djordje <neverloved.dev@gmail.com>
2024-08-23 17:09:42 +0200

    Add service repository

---
+++ TelegramBot/Interfaces/IServiceRepository.cs
@@ -0,0 +1,9 @@
+﻿using TelegramBot.Models;
+
+namespace TelegramBot.Interfaces;
+
+public interface IServiceRepository
+{
+    Task<Service> GetServiceByIdAsync(int id);
+    Task<IEnumerable<Service>> GetAllServicesAsync();
+}
