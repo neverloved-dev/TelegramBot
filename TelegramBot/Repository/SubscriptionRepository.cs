02f7fe7774717f1b6934a3d4ca726e16edce22fc
Andjic Djordje <neverloved.dev@gmail.com>
2024-08-24 11:19:23 +0200

    Add repository and controller for subscriptions

---
+++ TelegramBot/Repository/SubscriptionRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TelegramBot.Interfaces;
+using TelegramBot.Models;
+
+namespace TelegramBot.Repository;
+
+public class SubscriptionRepository:ISubscriptionRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public SubscriptionRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Subscription>> GetUserSubscriptionsAsync(int userId)
+    {
+        return await _context.Subscriptions
+            .Where(s => s.UserId == userId)
+            .ToListAsync();
+    }
+
+    public async Task<Subscription> GetSubscriptionByIdAsync(int id)
+    {
+        return await _context.Subscriptions.FindAsync(id);
+    }
+
+    public async Task AddSubscriptionAsync(Subscription subscription)
+    {
+        await _context.Subscriptions.AddAsync(subscription);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateSubscriptionAsync(Subscription subscription)
+    {
+        _context.Subscriptions.Update(subscription);
+        await _context.SaveChangesAsync();
+    }
+}
